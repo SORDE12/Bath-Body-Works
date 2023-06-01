72cbd7e6276c898479ec4b80eb9030e5371eff43
SORDE12 <sordeomkar2564@gmail.com>
2023-06-01 22:50:45 +0530

    added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
@@ -19,5 +18,3 @@ root.render(
   </BrowserRouter>
 );
 
-reportWebVitals();
-//https://project1-ysv4.onrender.com/
